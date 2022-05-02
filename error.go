d37a09b9185ce2c1c10080b55d4dd4dc28f74563
Feby Tanzil <feby.tanzil@gmail.com>
2022-05-02 16:52:06 +0800

    feat: add requeue custom delay support for nsq

--- error.go
+++ error.go
@@ -1,5 +1,7 @@
 package gobroker
 
+import "time"
+
 type DeadLetterError struct {
 	topic      string
 	errMessage string
@@ -15,4 +17,21 @@ func (d *DeadLetterError) GetTopic() string {
 
 func NewDeadLetterError(topic, errMessage string) *DeadLetterError {
 	return &DeadLetterError{topic: topic, errMessage: errMessage}
+}
+
+type DeferredError struct {
+	delay      time.Duration
+	errMessage string
+}
+
+func (d *DeferredError) Error() string {
+	return d.errMessage
+}
+
+func (d *DeferredError) GetDelay() time.Duration {
+	return d.delay
+}
+
+func NewDeferredError(delay time.Duration, errMessage string) *DeferredError {
+	return &DeferredError{delay: delay, errMessage: errMessage}
 }
